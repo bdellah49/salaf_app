1dae27c98d68967ae7558d4b9058c7728b0267d7
abdellah beliacine <abdellahbelyacine49@gmail.com>
2023-02-25 17:42:03 +0100

    creatd the header componen added and styled it

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "../logos/logo1.png";
+import pin from "../logos/pin.png";
+import search from "../logos/search.png";
+import "../styles/header.css";
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <img className="logo" src={logo} alt="logo" />
+        <div className="search">
+          <input className="search_input" type="text" name="search" />
+          <img src={search} alt='search_icon' className="search_icon" />
+        </div>
+        <div className="pin">
+          <img className="pin_img" src={pin} alt="pin" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
